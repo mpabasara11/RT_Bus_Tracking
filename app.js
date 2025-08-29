312dfd03a0190d39296e462210bce14c63feac92
Malindu Pabasara <mpabasara11@gmail.com>
2025-08-29 13:20:29 +0530

    removed vercel and fixed some env variables

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const port = 8080
 const cors = require('cors');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
@@ -7,47 +8,64 @@ const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
 const { swaggerUi, swaggerSpec } = require('./swagger');
 
+//const jwtSecret = "secret"           //remember to change this
+
 require('dotenv').config();
 const jwtSecret = process.env.JWT_SECRET;
+const mongouri = process.env.MONGODB_URI;
 
-// importing routes
-const adminRoutes = require('./route_files/admin.js');
-const authenticationRoutes = require('./route_files/auth.js');
-const busOperatorRoutes = require('./route_files/busOperator.js');
-const commuterRoutes = require('./route_files/commuter.js');
 
-app.use(cors());
+//importing routes
+const adminRoutes = require('./route_files/admin.js')
+const authenticationRoutes = require('./route_files/auth.js')
+const busOperatorRoutes = require('./route_files/busOperator.js')
+const commuterRoutes = require('./route_files/commuter.js')
+
+
+app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
 app.use(cookieParser());
 
-// middleware for request logging
+
+//  middleware for request logging
 app.use((req, res, next) => {
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
     next();
 });
 
+
 // Swagger UI Route
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// connect auth routes
+
+
+//connect auth routes 
 app.use('/auth', authenticationRoutes);
 
-// connect commuter routes
+//connect commuter routes 
 app.use('/commuter', commuterRoutes);
 
-// middleware for cookie verification and token validation
+
+
+//middleware for cookie verification and tokel validation
 app.use((req, res, next) => {
+
+    //check if state_token cookie is present
     if (req.cookies.state_token) {
         console.log('cookie found');
 
+        //verify the token
         jwt.verify(req.cookies.state_token, jwtSecret, (error, decodedToken) => {
             if (error) {
                 console.log('Invalid token');
                 res.status(401).json({ error: 'Invalid token' });
-            } else {
+                //  next();
+            }
+            else {
                 console.log('Token verified');
 
-                // attach token details to request object
+                //check the detail inside token and put them inside request object
+
                 req.userName = decodedToken.userName;
                 req.userRole = decodedToken.userRole;
                 req.firstName = decodedToken.firstName;
@@ -56,19 +74,60 @@ app.use((req, res, next) => {
                 req.nic = decodedToken.nic;
 
                 next();
+
+
             }
         });
-    } else {
+    }
+    else {
         console.log('cookie not found');
         res.status(401).json({ error: 'cookie not found' });
+
+
     }
+
 });
 
-// connect admin routes
+
+//connect admin routes 
 app.use('/admin', adminRoutes);
 
-// connect bus operator routes
+//connect bus operator routes 
 app.use('/busOperator', busOperatorRoutes);
 
-// Export app for serverless
-module.exports = app;
+
+
+
+
+connectToDb();
+
+
+
+
+//////////////////////////////////////////////////////////////   Function definitions ///////////////////////////////////////////////////////////////////////////////
+
+
+//entablish the connection with database
+async function connectToDb() {
+    // const uri = "mongodb+srv://mpabasara11:zxasd@cluster0.sktjibw.mongodb.net/RtBusTrackingApi?retryWrites=true&w=majority";
+
+    try {
+        await mongoose.connect(mongouri);
+        console.log("Connected to mongoDB");
+        startServer();
+    }
+    catch (error) {
+        console.error(error);
+
+
+    }
+
+
+}
+
+
+function startServer() {
+    app.listen(port, () => {
+        console.log(`Server is running on port: ${port}`);
+    });
+}
