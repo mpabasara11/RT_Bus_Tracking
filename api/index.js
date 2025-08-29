55354fb9153ea8470b65c1f7d4a4756534b262c3
Malindu Pabasara <mpabasara11@gmail.com>
2025-08-29 12:32:32 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,6 +3,11 @@ const app = require('../app');
 const connectToDb = require('../db');
 
 module.exports.handler = async (req, res) => {
-    await connectToDb(); // ensure DB connection
-    return serverless(app)(req, res);
+    try {
+        await connectToDb();        // ensure DB connected
+        return serverless(app)(req, res);
+    } catch (err) {
+        console.error("Serverless function error:", err);
+        res.status(500).send("Internal Server Error");
+    }
 };
