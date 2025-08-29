5f1ad0ddd12bcae6422d2f9b894400b7c88a2655
Malindu Pabasara <mpabasara11@gmail.com>
2025-08-29 19:10:46 +0530

    swagger base url change

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
         },
         servers: [
             {
-                url: 'http://localhost:8080', // your API base URL
+                url: 'https://rt-bus-tracking.vercel.app/', // your API base URL
             },
         ],
     },
